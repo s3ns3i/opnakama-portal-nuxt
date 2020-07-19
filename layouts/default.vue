318ac45fc8656173436897a41c5f0b4526943688
s3ns3i <eragonsm@gmail.com>
2020-07-19 20:10:46 +0200

    Refactoring

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,18 +29,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-btn fixed right bottom fab color="primary">
       <v-icon>mdi-chat</v-icon>
     </v-btn>
@@ -51,22 +39,5 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-    }
-  },
-}
+export default {}
 </script>
