1eb1f8386d7da2539d222e22b9e672623e7ebbc4
s3ns3i <eragonsm@gmail.com>
2020-08-05 23:53:18 +0200

    Showing latest OP chapters

--- components/app-info.vue
+++ components/app-info.vue
@@ -1,12 +1,60 @@
 <template>
   <v-card>
-    <v-card-title>Info</v-card-title>
-    <v-card-text>hello from info</v-card-text>
+    <v-card-title>Rozdziały One Piece</v-card-title>
+    <v-card-text>
+      <div class="info__container">
+        <div
+          v-for="chapter in chapters"
+          :key="chapter.id"
+          class="info__container__btn"
+          block
+          text
+        >
+          <v-row>
+            <v-col cols="3">{{ chapter.chapter }}</v-col>
+            <v-col class="chapter-name" cols="9">{{ chapter.name }}</v-col>
+          </v-row>
+        </div>
+      </div>
+    </v-card-text>
   </v-card>
 </template>
 
 <script>
 export default {
   name: 'AppInfo',
+  data() {
+    return {
+      chapters: [],
+    }
+  },
+  beforeMount() {
+    this.fetchChapters()
+  },
+  methods: {
+    fetchChapters() {
+      this.$axios
+        .get('http://localhost:3002/chapters?stub=piraciop')
+        .then(({ data }) => {
+          this.chapters = data
+        })
+    },
+  },
 }
 </script>
+<style lang="scss" scoped>
+.info {
+  &__container {
+    display: flex;
+    flex-direction: column;
+    &__btn {
+      font-size: 1rem;
+    }
+  }
+}
+.chapter-name {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
