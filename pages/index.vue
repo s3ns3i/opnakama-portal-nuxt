a3f8b7bf7fa9d885df6539794705ce18b139edf2
s3ns3i <eragonsm@gmail.com>
2020-08-05 23:53:56 +0200

    Changed api key

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,13 @@
     <v-col cols="12" sm="6" md="4" lg="4" xl="3"
       ><app-feed
         title="Aktualności OP nakama"
-        app-id="902476783583400"
+        app-id="1067347306994131"
         feed-id="onepiecenakamapl"
     /></v-col>
     <v-col cols="12" sm="6" md="4" lg="4" xl="3"
       ><app-feed
         title="Aktualności Wanted Team"
-        app-id="902476783583400"
+        app-id="1067347306994131"
         feed-id="Wanted-Team-1219982108141916"
     /></v-col>
   </v-row>
